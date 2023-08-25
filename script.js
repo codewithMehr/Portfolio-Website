da098b60ae17056e978d1fa09552a19acaf772ba
Mehr Hussain <90125165+codewithMehr@users.noreply.github.com>
2023-08-26 00:33:25 +0500

    adding react project

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ let darkMode = document.getElementById("darkMode");
 darkMode.addEventListener('click', () => {
     document.getElementById("background").classList.toggle("background2");
     document.getElementById("darkMode").innerHTML = "&#9728;"
-    pic.src = "img/profile-pic2.png";
+    pic.src = "img/testing7.png";
     audio.play();
     if (document.getElementById("background").classList.contains("background2")) {
         document.getElementById("darkMode").title = "light Mode";
     } else {
         document.getElementById("darkMode").title = "Dark Mode";
         document.getElementById("darkMode").innerHTML = "&#9790;"
-        pic.src = "img/round.png";
+        pic.src = "img/testing6.png";
     }
 })
 
 let darkMode2 = document.getElementById("darkMode-2");
 darkMode2.addEventListener('click', () => {
     document.getElementById("background").classList.toggle("background2");
-    pic.src = "img/profile-pic2.png";
+    pic.src = "img/testing7.png";
     document.getElementById("darkMode-2").innerHTML = "&#9728;"
     document.getElementById("darkMode-2").title = "light Mode";
     audio.play();
@@ -33,7 +33,7 @@ darkMode2.addEventListener('click', () => {
     } else {
         document.getElementById("darkMode-2").title = "Dark Mode";
         document.getElementById("darkMode-2").innerHTML = "&#9790;"
-        pic.src = "img/round.png";
+        pic.src = "img/testing6.png";
     }
 })
 
